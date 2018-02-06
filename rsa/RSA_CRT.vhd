82612414f60b4d244dc38af5fe120ec39c3ebda7
kboyeldieu <30385311+kboyeldieu@users.noreply.github.com>
2018-02-06 10:15:41 +0100

    Update RSA_CRT.vhd

--- rsa/RSA_CRT.vhd
+++ rsa/RSA_CRT.vhd
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date:    10:08:54 02/06/2018 
+-- Create Date:    10:08:13 02/06/2018 
 -- Design Name: 
--- Module Name:    modinv - Behavioral 
+-- Module Name:    RSA_CRT - Behavioral 
 -- Project Name: 
 -- Target Devices: 
 -- Tool versions: 
@@ -19,20 +19,34 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+entity RSA_CRT is
+	Generic (KEYSIZE: integer := 40);
+    Port (cypher: out std_logic_vector(KEYSIZE-1 downto 0);
+			 clk: in std_logic;
+			 ds: in std_logic;
+			 reset: in std_logic;
+			 ready: out std_logic
+			 );
+end RSA_CRT;
 
-entity modinv is
-end modinv;
+architecture Behavioral of RSA_CRT is
 
-architecture Behavioral of modinv is
+component modmult is
+	Generic (MPWID: integer);
+    Port ( mpand : in std_logic_vector(MPWID-1 downto 0);
+           mplier : in std_logic_vector(MPWID-1 downto 0);
+           modulus : in std_logic_vector(MPWID-1 downto 0);
+           product : out std_logic_vector(MPWID-1 downto 0);
+           clk : in std_logic;
+           ds : in std_logic;
+			  reset : in std_logic;
+			  ready: out std_logic);
+end component;
 
 begin
 
