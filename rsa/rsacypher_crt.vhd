3d460aec89364e7973ddb1e5b9170f56d95a1f9f
kboyeldieu <30385311+kboyeldieu@users.noreply.github.com>
2018-02-06 15:45:41 +0100

    Update rsacypher_crt.vhd

--- rsa/rsacypher_crt.vhd
+++ rsa/rsacypher_crt.vhd
@@ -1,32 +1,12 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    10:08:13 02/06/2018 
--- Design Name: 
--- Module Name:    RSA_CRT - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-
 entity RSA_CRT is
 	Generic (KEYSIZE: integer := 40);
-    Port (cypher: out std_logic_vector(KEYSIZE-1 downto 0);
+    Port (plaintext: out std_logic_vector(KEYSIZE-1 downto 0);
 			 clk: in std_logic;
 			 ds: in std_logic;
 			 reset: in std_logic;
@@ -60,41 +40,48 @@ component exponentiation is
 			  ready: out std_logic);
 end component;
 
-signal modreg: std_logic_vector(KEYSIZE-1 downto 0);	-- store the modulus value during operation
 signal modin: std_logic_vector(KEYSIZE-1 downto 0);	-- value to get the modulus
 signal modout: std_logic_vector(KEYSIZE-1 downto 0);	-- result of modulus
-signal one: std_logic_vector(KEYSIZE-1 downto 0);	-- constant 1
+signal modmod: std_logic_vector(KEYSIZE-1 downto 0);	-- store the modulus value during operation
+signal one: std_logic_vector(KEYSIZE-1 downto 0);	   -- constant 1 (beneath its a modular multiplication by 1)
 
+signal prodin1: std_logic_vector(KEYSIZE-1 downto 0);	-- first operand of product
+signal prodin2: std_logic_vector(KEYSIZE-1 downto 0);	-- second operand of product
+signal prodout: std_logic_vector(KEYSIZE-1 downto 0);	-- result of modular product
+signal prodmod: std_logic_vector(KEYSIZE-1 downto 0);	-- modulus of product
+
+signal expin: std_logic_vector(KEYSIZE-1 downto 0);	-- value to get the exponentiation
 signal expout: std_logic_vector(KEYSIZE-1 downto 0);	-- result of exponentiation
-signal expin: std_logic_vector(KEYSIZE-1 downto 0);	-- result of exponentiation
-signal expexp: std_logic_vector(KEYSIZE-1 downto 0);	-- result of exponentiation
-signal expmod: std_logic_vector(KEYSIZE-1 downto 0);	-- result of exponentiation
+signal expexp: std_logic_vector(KEYSIZE-1 downto 0);	-- exponent of exponentiation
+signal expmod: std_logic_vector(KEYSIZE-1 downto 0);	-- modulus of exponentiation
 
-signal modrdy, exprdy: std_logic;	-- signals to indicate completion of modulus, exponentiation
-signal modgo, expgo: std_logic;	-- signals to trigger start of modulus, exponentiation
+signal modinvin: std_logic_vector(KEYSIZE-1 downto 0);	-- value to get the modular inverse
+signal modinvout: std_logic_vector(KEYSIZE-1 downto 0);	-- result of modular inverse
+signal modinvexp: std_logic_vector(KEYSIZE-1 downto 0);	-- exponent of modular inverse
+signal modinvmod: std_logic_vector(KEYSIZE-1 downto 0);	-- modulus of modular inverse
+
+signal modrdy, prodrdy, exprdy, modinvrdy: std_logic;	-- signals to indicate completion of modulus, product, exponentiation, modular inverse
+signal modgo, prodgo, expgo, modinvgo: std_logic;	-- signals to trigger start of modulus, product, exponentiation, modular inverse
 signal done: std_logic;	-- signal to indicate encryption complete
 
-signal cpt: INTEGER RANGE 0 to 7; -- signal to track the current operation to perform
+signal step: INTEGER RANGE 0 to 7; -- signal to track the current operation to perform
 
-signal dp, dq, sp, sq: std_logic_vector(KEYSIZE-1 downto 0); -- temporal results 
+signal iq, dp, dq, sp, sq: std_logic_vector(KEYSIZE-1 downto 0); -- temporal results 
+signal tmp_large: std_logic_vector(KEYSIZE*2-1 downto 0); -- signal to store temporal large products
 
 -- constants
-signal inData: std_logic_vector(KEYSIZE-1 downto 0);
-signal inExp: std_logic_vector(KEYSIZE-1 downto 0);
-signal inMod: std_logic_vector(KEYSIZE-1 downto 0);
-signal p, q: std_logic_vector(KEYSIZE-1 downto 0); -- factors of the modulus inMod
+signal c: std_logic_vector(KEYSIZE-1 downto 0); -- data to crypt/decrypt
+signal p, q: std_logic_vector(KEYSIZE-1 downto 0); -- factors of the modulus
 signal d: std_logic_vector(KEYSIZE-1 downto 0); -- private key
 
 begin
 	
 	-- initialize constants
-	inData <= x"03273923ff";
-	inExp <= x"02103f7fff";
-	inMod <= x"0ef03f7fff";
+	c <= x"03273923ff";
 	p <= x"0ef03f7fff";
 	q <= x"0ef03f7fff";
 	d <= x"0ef03f7fff";
-	one <= "1";
+	one <= x"0000000001";
 	ready <= done;
 	
 	-- Modular multiplier to compute modulus
@@ -102,13 +89,26 @@ begin
 	Generic Map(MPWID => KEYSIZE)
 	Port Map(mpand => modin,
 				mplier => one,
-				modulus => modreg,
+				modulus => modmod,
 				product => modout,
 				clk => clk,
 				ds => modgo,
 				reset => reset,
 				ready => modrdy);
+	
+	-- Modular multiplier to produce products
+	product: modmult
+	Generic Map(MPWID => KEYSIZE)
+	Port Map(mpand => prodin1,
+				mplier => prodin2,
+				modulus => prodmod,
+				product => prodout,
+				clk => clk,
+				ds => prodgo,
+				reset => reset,
+				ready => prodrdy);
 				
+	-- Exponentiation		
 	expo: exponentiation
 	Generic Map(KEYSIZE => KEYSIZE)
 	Port Map(inData => expin,
@@ -120,16 +120,28 @@ begin
 				reset => reset,
 				ready => exprdy);
 				
+	-- Modular inverse
+	--modinv: modinv
+	--Generic Map(KEYSIZE => KEYSIZE)
+	--Port Map(inData => expin,
+	--			inExp => expexp,
+	--			inMod => expmod,
+	--       exponentiation => expout,
+	--			clk => clk,
+	--			ds => expgo,
+	--			reset => reset,
+	--			ready => exprdy);
+				
 	crypto: process (clk, reset, done, ds) is
 	begin
 		
 		if reset = '1' then
 			done <= '1';
-			modin <= (others => '0');
-			expin <= (others => '0');
 			modgo <= '0';
+			prodgo <= '0';
 			expgo <= '0';
-			cpt <= 0;
+			step <= 1;
+			iq <= (others => '0');
 			dp <= (others => '0');
 			dq <= (others => '0');
 			sp <= (others => '0');
@@ -140,30 +152,85 @@ begin
 			if done = '1' then
 				if ds = '1' then
 					done <= '0';
-					cpt <= 1;
+					step <= 1;
 				end if;
 				
-			elsif cpt = 1 then
+			elsif step = 1 then
 			-- compute dp = d mod (p-1)
 				if modrdy = '1' then
-					cpt <= cpt + 1;
+					step <= step + 1;
+					dp <= modout;
+					modgo <= '0';
 				else
-					modreg <= p - 1;
+					modmod <= p - 1;
 					modin <= d;
 					modgo <= '1';
 				end if;
-			elsif cpt = 2 then
+				
+			elsif step = 2 then
 			-- compute dq = d mod (q-1)
-			
-			elsif cpt = 3 then
+				if modrdy = '1' then
+					step <= step + 1;
+					dq <= modout;
+					modgo <= '0';
+				else
+					modmod <= q - 1;
+					modin <= d;
+					modgo <= '1';
+				end if;
+				
+			elsif step = 3 then
 			-- compute iq = q^-1 mod p
-			
-			elsif cpt = 4 then
-			-- compute sp = m^dp mod p
-			
-			elsif cpt = 5 then
-			-- compute sq = m^dq mod q
-			
+				if modinvrdy = '1' then
+					step <= step + 1;
+					iq <= modinvout;
+					modinvgo <= '0';
+				else
+					modinvmod <= p;
+					modinvin <= q;
+					modinvgo <= '1';
+				end if;
+				
+			elsif step = 4 then
+			-- compute sp = c^dp mod p
+				if exprdy = '1' then
+					step <= step + 1;
+					sp <= expout;
+					expgo <= '0';
+				else
+					expexp <= dp;
+					expmod <= p;
+					expin <= c;
+					expgo <= '1';
+				end if;
+				
+			elsif step = 5 then
+			-- compute sq = c^dq mod q
+				if exprdy = '1' then
+					step <= step + 1;
+					sq <= expout;
+					expgo <= '0';
+				else
+					expexp <= dq;
+					expmod <= q;
+					expin <= c;
+					expgo <= '1';
+				end if;
+
+			elsif step = 6 then
+			-- compute plaintext = sq + q(iq(sp - sq) mod p)
+				if modrdy = '1' then
+					tmp_large <= sq + q * expout;
+					plaintext <= tmp_large(KEYSIZE-1 downto 0);
+					done <= '1';
+					modgo <= '0';
+				else
+					modmod <= p;
+					tmp_large <= iq * (sp - sq);
+					modin <= tmp_large(KEYSIZE-1 downto 0);
+					modgo <= '1';
+				end if;
+
 			end if;
 				
 		end if;
@@ -171,4 +238,3 @@ begin
 	end process crypto;
 	
 end Behavioral;
-
