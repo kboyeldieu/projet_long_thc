dc01bc844493248f85f829db26040c7dc1d4a482
titoukiw <titouan.tanguy@gmail.com>
2018-02-13 10:48:53 +0100

    Update Nexys4RSA.vhd

--- rsa/Nexys4RSA.vhd
+++ rsa/Nexys4RSA.vhd
@@ -4,27 +4,21 @@ use IEEE.std_logic_arith.all;
 use IEEE.std_logic_unsigned.all;
 
 entity Nexys4RSA is
-  port (
-    -- les 2 boutons
-    btnC : in std_logic;
-    btnU : in std_logic;
-    -- horloge
-    clk : in std_logic;
-    -- les 2 leds
-    led : out std_logic_vector (1 downto 0);
-  );
+  port ( btnC : in std_logic; -- les 2 boutons
+         btnU : in std_logic;
+         clk : in std_logic; -- horloge
+         led : out std_logic_vector (1 downto 0); -- les 2 leds);
 end Nexys4RSA;
 
 architecture synthesis of Nexys4RSA is
 
 component RSACypher is
   Generic (KEYSIZE: integer := 40);
-    Port (plaintext: out std_logic_vector(KEYSIZE-1 downto 0);
-       clk: in std_logic;
-       ds: in std_logic;
-       reset: in std_logic;
-       ready: out std_logic
-       );
+  port ( plaintext: out std_logic_vector(KEYSIZE-1 downto 0);
+         clk: in std_logic;
+         ds: in std_logic;
+         reset: in std_logic;
+         ready: out std_logic);
 end component;
 
 signal plaintext: std_logic_vector(39 downto 0);
@@ -34,12 +28,11 @@ begin
   -- 2 leds éteintes
   led(1 downto 0) <= (others => '0');
   
-  rsa: RSAcypher PORT MAP(
-    plaintext => plaintext,
-    clk => clk,
-    ds => btnU,
-    reset => btnC,
-    ready => led(0)
-  );
+  rsa: RSAcypher 
+  PORT MAP( plaintext => plaintext,
+            clk => clk,
+            ds => btnU,
+            reset => btnC,
+            ready => led(0));
 
 end synthesis;
