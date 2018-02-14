e98d0ee124facc97862fe33dd687fe032d61d67a
kboyeldieu <30385311+kboyeldieu@users.noreply.github.com>
2018-02-14 15:15:48 +0100

    Update and rename Nexys4RSA.vhd to spartan3_rsa.vhd

--- rsa/spartan3_rsa.vhd
+++ rsa/spartan3_rsa.vhd
@@ -4,10 +4,10 @@ use IEEE.std_logic_arith.all;
 use IEEE.std_logic_unsigned.all;
 
 entity Nexys4RSA is
-  port ( btnC : in std_logic; -- les 2 boutons
+  port ( btnC : in std_logic;
          btnU : in std_logic;
-         clk : in std_logic; -- horloge
-         led : out std_logic_vector (1 downto 0); -- les 2 leds);
+         clk : in std_logic;
+         led : out std_logic;
 end Nexys4RSA;
 
 architecture synthesis of Nexys4RSA is
@@ -24,15 +24,12 @@ end component;
 signal plaintext: std_logic_vector(39 downto 0);
 
 begin
-
-  -- 2 leds éteintes
-  led(1 downto 0) <= (others => '0');
-  
+ 
   rsa: RSAcypher 
   PORT MAP( plaintext => plaintext,
             clk => clk,
             ds => btnU,
             reset => btnC,
-            ready => led(0));
+            ready => led);
 
 end synthesis;
