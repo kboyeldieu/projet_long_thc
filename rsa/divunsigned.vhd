294df9509748b0a85182d3fd720c87a8c98f49e9
titoukiw <titouan.tanguy@gmail.com>
2018-02-20 11:43:06 +0100

    Update divunsigned.vhd

--- rsa/divunsigned.vhd
+++ rsa/divunsigned.vhd
@@ -1,36 +1,8 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    10:57:15 02/06/2018 
--- Design Name: 
--- Module Name:    divunsigned - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity divunsigned is
 Generic (MPWID: integer);
