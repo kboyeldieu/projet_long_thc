22fad3238b7ceee92635c09f0e7bfe958c77154a
titoukiw <titouan.tanguy@gmail.com>
2018-03-05 15:27:27 +0100

    Update chall.py

--- src/rsa_crt/chall.py
+++ src/rsa_crt/chall.py
@@ -3,7 +3,7 @@ print("Find P and Q such that P*Q = N")
 while(True):
 	p = input("Enter your value for P ")
 	q = input("Enter your value for Q ")
-	if( p*q == 0x2fa47df91c352f3112cb and p != 0x2fa47df91c352f3112cb and q != 0x2fa47df91c352f3112cb) :
+	if( p*q == 0x2fa47df91c352f3112cb and p != 0x2fa47df91c352f3112cb and q != 0x2fa47df91c352f3112cb and p >= 1 and q >= 1) :
 		print("Well done, you've broken RSA-CRT")
 		break
 	else :
